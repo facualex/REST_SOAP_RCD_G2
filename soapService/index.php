46dcc6c8fd8d41cbac7565e5d07aedb3674f9d63
Facundo <facualex@Facundos-MacBook-Air.local>
2021-04-26 23:52:59 -0400

    Estructura jerarquica solicitada respondiendo adecuadamente

--- soapService/index.php
+++ soapService/index.php
@@ -9,23 +9,40 @@ $server->configureWSDL('SOAP Names Web Service', $namespace);
 $server->schemaTargetNamespace = $namespace;
 
 $server->wsdl->addComplexType(
+    'nameArray',
+    'complexType',
+    'array',
+    '',
+    'SOAP-ENC:Array',
+    array(),
+    array(
+        array(
+            'ref' => 'SOAP-ENC:arrayType',
+            'wsdl:arrayType' => 'xsd:string[]'
+        )
+    ),
+    'xsd:string'
+);
+
+$server->wsdl->addComplexType(
     'userNames',
     'complexType',
     'struct',
     'sequence',
     '',
     array(
-        'names' => array('name' => 'names', 'type' => 'xsd:string'),
-        'lastNames' => array('name' => 'lastNames', 'type' => 'xsd:string'),
+        'names' => array('name' => 'names', 'type' => 'tns:nameArray'),
+        'lastNames' => array('name' => 'lastNames', 'type' => 'tns:nameArray'),
     )
 );
+
 
 $server->register("splitNames", array("name" => "xsd:string"), array("return" => "tns:userNames"), $namespace);
 
 function splitNames($names) {
     return array(
-        'names' => 'Facundo',
-        'lastNames' => 'Alexandre Buchelli'
+        'names' => array('Facundo'),
+        'lastNames' => array('Alexandre', 'Buchelli')
     );
 
     // Split string names, get two last. Those are the last names. The rest are names
